77885ee8d11442ecd1afabab2d8220918baa19d7
FinalDuty <finalduty@github.com>
2015-04-05 18:23:55 +1200

    Added install script for named

---
+++ svc/install-dns.sh
@@ -0,0 +1,20 @@
+## Script for Install of DNS Service
+#!/bin/bash
+
+yum update -y; yum install -y bind bind-utils
+
+groupadd ddns
+gpasswd -a named ddns
+gpasswd -a dhcpd ddns
+
+dnsseckey=`dnssec-keygen -a HMAC-MD5 -b 128 -r /dev/urandom -n USER DDNS`
+awk '{print $7}' "$dnsseckey".key > /etc/ddns.key
+
+chown root.ddns /etc/ddns.key
+chmod 640 /etc/ddns.key
+
+mkdir /var/named/zones
+
+curl -L https://raw.githubusercontent.com/finalduty/enl/master/svc/named.conf > /etc/named.conf
+curl -L https://raw.githubusercontent.com/finalduty/enl/master/svc/172.20.2 > /var/named/zones/172.20.2
+curl -L https://raw.githubusercontent.com/finalduty/enl/master/svc/finalduty.me > /var/named/zones/finalduty.me
