ea6a0b3aa9d58459d355688c2ac91dcab80ef588
FinalDuty <finalduty@github.com>
2015-04-26 12:22:28 +1200

    Changed test switch for 'wget'

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 
 [ -e /usr/bin/curl ] && bash <(curl https://raw.githubusercontent.com/finalduty/enl/master/install/firstrun.sh) && exit
-[ -f /usr/bin/wget ] && bash <(wget -q https://raw.githubusercontent.com/finalduty/enl/master/install/firstrun.sh) && exit
+[ -e /usr/bin/wget ] && bash <(wget -q https://raw.githubusercontent.com/finalduty/enl/master/install/firstrun.sh) && exit
 
 echo "Can not find a curl or wget install and I'm not programmed to install either of them."
 echo "Failed"
