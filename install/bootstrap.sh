0850544f9c3910e5f6be34c2025e4732809888b5
FinalDuty <finalduty@github.com>
2015-04-26 12:21:11 +1200

    Corrected tests for commands

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -2,8 +2,8 @@
 ## Bootstrap Script for templates to pull latest firstrun.sh
 #!/bin/bash
 
-[ -f `which curl` ] && bash <(curl https://raw.githubusercontent.com/finalduty/enl/master/install/firstrun.sh) && exit
-[ -f `which wget` ] && bash <(wget -q https://raw.githubusercontent.com/finalduty/enl/master/install/firstrun.sh) && exit
+[ -e /usr/bin/curl ] && bash <(curl https://raw.githubusercontent.com/finalduty/enl/master/install/firstrun.sh) && exit
+[ -f /usr/bin/wget ] && bash <(wget -q https://raw.githubusercontent.com/finalduty/enl/master/install/firstrun.sh) && exit
 
 echo "Can not find a curl or wget install and I'm not programmed to install either of them."
 echo "Failed"
